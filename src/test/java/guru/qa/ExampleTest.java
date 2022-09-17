ee55dbd7f9f4fbd16e5f320e30a6eece15481ec2
Ксения <guzgks@gmail.com>
2022-09-18 01:21:13 +0300

    Init commit

--- src/test/java/guru/qa/ExampleTest.java
+++ src/test/java/guru/qa/ExampleTest.java
@@ -4,8 +4,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ExampleTest {
+
     @Test
-    void exampleTest(){
+    void exampleTest0(){
         Assertions.assertTrue(2>1);
+    }
+
+    @Test
+    void exampleTest1(){
+        Assertions.assertTrue(3>2);
     }
 }
